b79a089c555e28abb70d31918dc7ceed36fa20ae
ahvdk <50673620+ahvdk@users.noreply.github.com>
2019-06-04 16:10:50 -0700

    Delete queue.py

--- ssnmods/queue.py
+++
@@ -1,9 +0,0 @@
-from ssnmods import gvars as g
-
-def pop_queue(signal=0):
-   if ((g.SSN_QUEUE[0] == "stop") or (signal == "stop")):
-      g.SSN_QUEUE = ["start_clean", "run_stop", "end_stop", "terminated"]
-   elif (signal == "error"):
-      g.SSN_QUEUE = ["error"]
-   else:
-      g.SSN_QUEUE.pop(0)
