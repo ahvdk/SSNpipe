f8a8042045498fd95e9bf97298c31fbcbe278dfd
ahvdk <50673620+ahvdk@users.noreply.github.com>
2019-06-04 15:07:14 -0700

    Add files via upload

---
+++ removetmp.py
@@ -0,0 +1,16 @@
+import threading
+from shutil import copyfile
+from ssnmods import gvars as g
+from ssnmods import (mods, queue)
+from ssnmods import settings as ssn
+
+class start_clean(threading.Thread):
+      
+   def __init__(self):
+      threading.Thread.__init__(self)
+      self.daemon = True
+      
+
+   def run(self):
+      #rmtree(mods.get_file("tmp"))
+      queue.pop_queue()
